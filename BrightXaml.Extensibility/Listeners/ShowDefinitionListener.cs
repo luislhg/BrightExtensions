77ca8ce94760b0cab233044e88a340db0b02c1b5
Luis Goll <luislhg@hotmail.com>
2025-11-12 01:34:03 -0300

    GoToBinding definition now relies on Changed lister and works for RelayCommand and ObservableProperty consistently

--- BrightXaml.Extensibility/Listeners/ShowDefinitionListener.cs
+++ BrightXaml.Extensibility/Listeners/ShowDefinitionListener.cs
@@ -11,7 +11,7 @@ using Range = Microsoft.VisualStudio.RpcContracts.Utilities.Range;
 
 namespace BrightXaml.Extensibility.Listeners;
 [VisualStudioContribution]
-public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
+public partial class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener, ITextViewChangedListener
 {
     private readonly TraceSource traceSource;
     private readonly SettingsService settingsService;
@@ -28,6 +28,8 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
     {
         this.traceSource = traceSource;
         this.settingsService = settingsService;
+
+        Debug.WriteLine("ShowDefinitionListener initialized.");
     }
 
     public Task TextViewClosedAsync(ITextViewSnapshot textView, CancellationToken cancellationToken)
@@ -35,25 +37,54 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
         return Task.CompletedTask;
     }
 
-    public async Task TextViewOpenedAsync(ITextViewSnapshot textView, CancellationToken cancellationToken)
+    public Task TextViewOpenedAsync(ITextViewSnapshot textView, CancellationToken cancellationToken)
+    {
+        // Check if the feature is enabled.
+        if (!settingsService.Data.GoToBinding.IsEnabled)
+            return Task.CompletedTask;
+
+        // TODO: Implement logic on Opened event to prevent TextViewChangedAsync from keep executing for a
+        // document the user is interacting with and we have already processed.
+
+        // Check if it's a generated document (MVVM Toolkit Source Generator) and queue the URI?
+        //if (textView?.Document?.Uri?.Segments?.Contains("VSGeneratedDocuments/") == true)
+        //    currentFileUri = textView?.Document?.Uri;
+
+        //await ApplyAsync(textView, cancellationToken);
+        return Task.CompletedTask;
+    }
+
+    public async Task TextViewChangedAsync(TextViewChangedArgs args, CancellationToken cancellationToken)
     {
         // Check if the feature is enabled.
         if (!settingsService.Data.GoToBinding.IsEnabled)
             return;
 
+        await ApplyAsync(args.AfterTextView, cancellationToken);
+    }
+
+    public async Task OpenFileAsync(string fileName, int lineStart, int columnStartOffset, CancellationToken cancellationToken)
+    {
+        var options = new OpenDocumentOptions(activate: true, selection: new Range(lineStart, columnStartOffset, 0, 0));
+        //var options = new OpenDocumentOptions(activate: true, ensureVisible: new Range(caretLine, 0, 0, 0), ensureVisibleOptions: EnsureRangeVisibleOptions.MinimumScroll);
+        await Extensibility.Documents().OpenTextDocumentAsync(new Uri(fileName), options, cancellationToken);
+    }
+
+    public async Task ApplyAsync(ITextViewSnapshot textView, CancellationToken cancellationToken)
+    {
         // Check if it's a generated document (MVVM Toolkit Source Generator)
         if (textView?.Document?.Uri?.Segments?.Contains("VSGeneratedDocuments/") == true)
         {
             try
             {
-                Debug.WriteLine("TextViewOpenedAsync: VSGeneratedDocuments");
+                Debug.WriteLine("ShowDefinitionListener: VSGeneratedDocuments");
                 var sw = Stopwatch.StartNew();
 
                 // Get the text content of the document.
                 var textContent = new string(textView.Document.Text.ToArray());
 
                 // Extract the class name from the document.
-                Match match = Regex.Match(textContent, @"\bpartial class\s+(\w+)\b");
+                Match match = FindClassNameRegex().Match(textContent);
                 if (match.Success)
                 {
                     var className = match.Groups[1].Value;
@@ -62,7 +93,7 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
                     // Check if the line is from a command.
                     if (textContent.Contains("CommunityToolkit.Mvvm.SourceGenerators.RelayCommandGenerator"))
                     {
-                        Debug.WriteLine("TextViewOpenedAsync: RelayCommandGenerator");
+                        Debug.WriteLine("ShowDefinitionListener: RelayCommandGenerator");
 
                         // Extract the method name.
                         var methodName = textView.Document.Uri.Segments.LastOrDefault().Replace(".g.cs", string.Empty).Split(".").LastOrDefault();
@@ -86,6 +117,7 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
                         var bindingWordLine = ShowDefinitionHelper.GetRelayCommandLine_MVVMToolkit(methodName, result);
                         var bindingStartOffsetFromLine = ShowDefinitionHelper.GetRelayCommandOffsetFromLine_MVVMToolkit(methodName, result);
                         var bindingWordEndOffsetFromLine = bindingStartOffsetFromLine + methodName.Length;
+                        traceSource.TraceEvent(TraceEventType.Information, 0, $"Opening {result} at line {bindingWordLine}, offset {bindingWordEndOffsetFromLine}");
                         await OpenFileAsync(result, bindingWordLine, bindingWordEndOffsetFromLine, cancellationToken);
 
                         // Close current document.
@@ -94,57 +126,61 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
                     // Check if the line is from a property.
                     else if (textContent.Contains("CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator"))
                     {
-                        Debug.WriteLine("TextViewOpenedAsync: ObservablePropertyGenerator");
-
-                        // TODO: API BUG?
-                        // Offsets only get a value at the first run, after they always have 0, thus this is not working as expected.
-                        Debug.WriteLine($"ActivePosition {textView.Selection.ActivePosition.Offset}");
-                        Debug.WriteLine($"InsertionPosition {textView.Selection.InsertionPosition.Offset}");
-                        Debug.WriteLine($"AnchorPosition {textView.Selection.AnchorPosition.Offset}");
+                        Debug.WriteLine("ShowDefinitionListener: ObservablePropertyGenerator");
 
                         // Extract the method or property name from the line, starting at the caret position and ending with a whitespace or breakline.
                         var line = textView.Document.GetLineFromPosition(textView.Selection.InsertionPosition);
                         var lineContent = new string(line.Text.ToArray());
                         var insertionPositionOffset = textView.Selection.InsertionPosition.Offset;
-                        var lineStartOffset = textContent.LastIndexOf('\n', insertionPositionOffset - 1) + 1;
-                        if (lineStartOffset == 0 && insertionPositionOffset == 0)
+                        if (insertionPositionOffset != 0)
                         {
-                            lineStartOffset = 0;
+                            var lineStartOffset = textContent.LastIndexOf('\n', insertionPositionOffset - 1) + 1;
+                            if (lineStartOffset == 0 && insertionPositionOffset == 0)
+                                lineStartOffset = 0;
+
+                            var lineCaretOffset = insertionPositionOffset - lineStartOffset;
+                            var bindingWord = ShowDefinitionHelper.GetWordAtCaret(lineContent, lineCaretOffset);
+                            Debug.WriteLine($"Binding word: {bindingWord}");
+
+                            // Extract the field name.
+                            var textPartial = textContent.Substring(insertionPositionOffset);
+                            const string textFind = "get => ";
+                            var fieldStartIndex = textPartial.IndexOf(textFind) + textFind.Length;
+                            var fieldEndIndex = textPartial.IndexOf(';', fieldStartIndex);
+                            string fieldName = textPartial.Substring(fieldStartIndex, fieldEndIndex - fieldStartIndex).Trim();
+                            Debug.WriteLine($"Field name: {fieldName}");
+
+                            // Find the file in the project.
+                            var viewModelBadPath = textView.Document.Uri.Segments.LastOrDefault().Replace($".g.cs", string.Empty);
+                            viewModelBadPath = viewModelBadPath.Replace(".", "\\") + ".cs";
+                            var viewModelFileName = Path.GetFileName(viewModelBadPath);
+                            var files = await Extensibility.Workspaces()
+                                            .QueryProjectsAsync(project => project
+                                                .Get(p => p.Files)
+                                                .Where(f => f.Path.EndsWith(viewModelFileName, StringComparison.InvariantCultureIgnoreCase))
+                                                .With(f => new { f.FileName, f.Path }), cancellationToken);
+
+                            var result = files.FirstOrDefault().Path;
+                            Debug.WriteLine($"ViewModel path: {result} ({sw.ElapsedMilliseconds}ms)");
+
+                            // Open the file where the binding is defined.
+                            var bindingWordOffset = ShowDefinitionHelper.GetObservablePropertyOffset_MVVMToolkit(fieldName, result);
+                            var bindingWordLine = ShowDefinitionHelper.GetObservablePropertyLine_MVVMToolkit(fieldName, result);
+                            var bindingStartOffsetFromLine = ShowDefinitionHelper.GetRelayCommandOffsetFromLine_MVVMToolkit(fieldName, result);
+                            var bindingWordEndOffsetFromLine = bindingStartOffsetFromLine + fieldName.Length;
+                            traceSource.TraceEvent(TraceEventType.Information, 0, $"Opening {result} at line {bindingWordLine}, offset {bindingWordEndOffsetFromLine}");
+                            await OpenFileAsync(result, bindingWordLine, bindingWordEndOffsetFromLine, cancellationToken);
+
+                            // Close current document.
+                            await Extensibility.Documents().CloseDocumentAsync(textView.Document.Uri, SaveDocumentOption.NoSave, cancellationToken);
                         }
-                        var lineCaretOffset = insertionPositionOffset - lineStartOffset;
-                        var bindingWord = ShowDefinitionHelper.GetWordAtCaret(lineContent, lineCaretOffset);
-                        Debug.WriteLine($"Binding word: {bindingWord}");
-
-                        // Extract the field name.
-                        var textPartial = textContent.Substring(insertionPositionOffset);
-                        const string textFind = "get => ";
-                        var fieldStartIndex = textPartial.IndexOf(textFind) + textFind.Length;
-                        var fieldEndIndex = textPartial.IndexOf(';', fieldStartIndex);
-                        string fieldName = textPartial.Substring(fieldStartIndex, fieldEndIndex - fieldStartIndex).Trim();
-                        Debug.WriteLine($"Field name: {fieldName}");
-
-                        // Find the file in the project.
-                        var viewModelBadPath = textView.Document.Uri.Segments.LastOrDefault().Replace($".g.cs", string.Empty);
-                        viewModelBadPath = viewModelBadPath.Replace(".", "\\") + ".cs";
-                        var viewModelFileName = Path.GetFileName(viewModelBadPath);
-                        var files = await Extensibility.Workspaces()
-                                        .QueryProjectsAsync(project => project
-                                            .Get(p => p.Files)
-                                            .Where(f => f.Path.EndsWith(viewModelFileName, StringComparison.InvariantCultureIgnoreCase))
-                                            .With(f => new { f.FileName, f.Path }), cancellationToken);
-
-                        var result = files.FirstOrDefault().Path;
-                        Debug.WriteLine($"ViewModel path: {result} ({sw.ElapsedMilliseconds}ms)");
-
-                        // Open the file where the binding is defined.
-                        var bindingWordOffset = ShowDefinitionHelper.GetObservablePropertyOffset_MVVMToolkit(fieldName, result);
-                        var bindingWordLine = ShowDefinitionHelper.GetObservablePropertyLine_MVVMToolkit(fieldName, result);
-                        var bindingStartOffsetFromLine = ShowDefinitionHelper.GetRelayCommandOffsetFromLine_MVVMToolkit(fieldName, result);
-                        var bindingWordEndOffsetFromLine = bindingStartOffsetFromLine + fieldName.Length;
-                        await OpenFileAsync(result, bindingWordLine, bindingWordEndOffsetFromLine, cancellationToken);
-
-                        // Close current document.
-                        await Extensibility.Documents().CloseDocumentAsync(textView.Document.Uri, SaveDocumentOption.NoSave, cancellationToken);
+                        else
+                        {
+                            // For TextViewOpenedAsync, Offsets only get a value at the first run, after they always have 0.
+                            // We need to rely on TextViewChangedAsync to get the correct offset.
+                            Debug.WriteLine("Insertion position offset is 0.");
+                            traceSource.TraceEvent(TraceEventType.Warning, 0, "Insertion position offset is 0 (probably Opened API)");
+                        }
                     }
                     else
                     {
@@ -159,7 +195,7 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
                 }
 
                 sw.Stop();
-                Debug.WriteLine($"ShowDefinitionListener ({sw.ElapsedMilliseconds}ms)");
+                Debug.WriteLine($"ShowDefinitionListener finished ({sw.ElapsedMilliseconds}ms)");
             }
             catch (Exception ex)
             {
@@ -169,10 +205,6 @@ public class ShowDefinitionListener : ExtensionPart, ITextViewOpenClosedListener
         }
     }
 
-    public async Task OpenFileAsync(string fileName, int lineStart, int columnStartOffset, CancellationToken cancellationToken)
-    {
-        var options = new OpenDocumentOptions(activate: true, selection: new Range(lineStart, columnStartOffset, 0, 0));
-        //var options = new OpenDocumentOptions(activate: true, ensureVisible: new Range(caretLine, 0, 0, 0), ensureVisibleOptions: EnsureRangeVisibleOptions.MinimumScroll);
-        await Extensibility.Documents().OpenTextDocumentAsync(new Uri(fileName), options, cancellationToken);
-    }
+    [GeneratedRegex(@"\bpartial class\s+(\w+)\b")]
+    private static partial Regex FindClassNameRegex();
 }
