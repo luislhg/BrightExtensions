c7a48a23476e8ce3d6c2f10d0f2023a90469ea3b
Luis Goll <luislhg@hotmail.com>
2026-08-15 02:46:57 -0300

    Fixed unit test project

--- BrightXaml.ExtensibilityTests/MockEntrypoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.VisualStudio.Extensibility;
-
-namespace BrightXaml.ExtensibilityTests;
-[VisualStudioContribution]
-internal class MockEntrypoint : Extension
-{
-    public override ExtensionConfiguration ExtensionConfiguration => null;
-}
